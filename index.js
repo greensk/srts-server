79a9d397d57d9bf2ff43312460a32d45cffeadab
Grigory Kosheev <grigory@kosheev.com>
2018-07-03 15:18:21 +0700

    feat game join, game start

--- index.js
+++ index.js
@@ -52,11 +52,52 @@ wsServer.on('request', function(request) {
             games.push({
               id: gameid,
               owner: client.id,
+              requests: [],
               name: client.name
             })
             client.status = 'owner'
             console.log(`create a new game name=${client.name} gameid=${gameid}`)
             sendGames()
+          }
+          return
+        }
+        if (messageData.type === 'joinGame') {
+          if (client.connection === connection) {
+            const gameid = payload.game
+            const gameObject = games.find(g => g.id === gameid)
+            if (gameObject) {
+              gameObject.requests.push({ name: client.name, id: client.id })
+              const owner = clients.find(c => c.id === gameObject.owner)
+              const message = {
+                type: 'setGameRequests',
+                payload: {
+                  list: gameObject.requests
+                }
+              }
+              owner.connection.sendUTF(JSON.stringify(message))
+            }
+          }
+          return
+        }
+        if (messageData.type === 'startGame') {
+          if (client.connection === connection) {
+            const gameObject = games.find(g => g.owner === client.id)
+            if (gameObject) {
+              client.game = gameObject.id
+              console.log(`add user id = ${client.id} to game id=${gameObject.id}`)
+              clients.forEach((currentClient) => {
+                if (currentClient.id === payload.clientId) {
+                  currentClient.game = gameObject.id
+                  console.log(`add user id = ${currentClient.id} to game id=${gameObject.id}`)
+                  const message = {
+                    type: 'startGame',
+                    payload: {}
+                  }
+                  currentClient.connection.sendUTF(JSON.stringify(message))
+                  console.log('start game')
+                }
+              })
+            }
           }
           return
         }
@@ -75,15 +116,18 @@ wsServer.on('request', function(request) {
     // close user connection
     const clientToRemove = clients.find(c => c.connection === connection)
     console.log(`remove user ${clientToRemove.id}`)
-    const gameToRemove = games.find(g => g.owner === clientToRemove)
+    console.log(games)
+    const gameToRemove = games.find(g => g.owner === clientToRemove.id)
     clients = clients.filter(c => c !== clientToRemove)
     if (gameToRemove) {
+      console.log(`remove game ${gameToRemove.id}`)
       clients.forEach((client) => {
         if (client.game === gameToRemove.id) {
           client.connection.close()
-          console.log(`remove game id=${gameToRemove.id}`)
+          console.log(`removed game id=${gameToRemove.id}`)
         }
       })
+      games = games.filter(g => g !== gameToRemove)
     }
   })
 })
